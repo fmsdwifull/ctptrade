45d335e5d877d047e66953d5a25f6e84ef5b620d
fmsdwifull <fmsdwifull@sina.com>
2018-05-27 21:06:15 +0800

    基于makefile的框架搭建完成，初步写了登录的步骤，下一步准备开发获取行情的模块

---
+++ samples/tradeapitest/tradeapitest.h
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+//#include <FtdcTraderApi.h>
+
+#include <ThostFtdcTraderApi.h>
+#include <ThostFtdcMdApi.h>
+#include <ThostFtdcUserApiDataType.h>
+#include <ThostFtdcUserApiStruct.h>
+
---
+++ src/login.h
@@ -0,0 +1,6 @@
+#ifndef _LOGIN_H__
+#define _LOGIN_H__
+
+int add(int x,
+
+#endif
